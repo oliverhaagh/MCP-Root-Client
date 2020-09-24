175251a7d98495afce268e1172c936bde17beb44
Oliver Haagh <ohaagh@gmail.com>
2020-09-24 17:20:07 +0200

    Some progress

--- src/main/java/net/maritimeconnectivity/rootcalist/client/Main.java
+++ src/main/java/net/maritimeconnectivity/rootcalist/client/Main.java
@@ -16,9 +16,66 @@
 
 package net.maritimeconnectivity.rootcalist.client;
 
+import io.swagger.client.ApiException;
+import io.swagger.client.api.RootCaControllerApi;
+import io.swagger.client.model.RootCA;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        Options options = setupOptions();
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            log.error("Parsing failed.", e);
+        }
+    }
 
+    public static Options setupOptions() {
+        Options options = new Options();
+
+        // Help
+        options.addOption("h", "help", false, "Show this help message");
+
+        return options;
+    }
+
+    public static void createRootCA(String certPath) {
+        File certFile = new File(certPath);
+        if (certFile.exists()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            try (Stream<String> lines = Files.lines(certFile.toPath(), StandardCharsets.UTF_8)) {
+                lines.forEach(stringBuilder::append);
+            } catch (IOException e) {
+                log.error("Could not open file");
+            }
+            String certificate = stringBuilder.toString();
+            RootCaControllerApi rootCaControllerApi = new RootCaControllerApi();
+            try {
+                RootCA rootCA = rootCaControllerApi.createRootCAUsingPOST(certificate);
+                if (rootCA != null) {
+                    log.info("New root CA was successfully created:\n{}", rootCA.toString());
+                }
+            } catch (ApiException e) {
+                log.error("Creation of root CA failed with following message {} and HTTP status code {}", e.getResponseBody(), e.getCode());
+            }
+        } else {
+            log.error("File {} does not exist", certPath);
+        }
     }
 }
